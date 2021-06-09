fe78ebd7e4b5fe8a8f26d1e932cf1f11572efb29
Patricia Nobre <2108le@gmail.com>
2021-06-09 14:05:24 -0300

    connection string to mongo working

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.post(
        if(!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
        }
-       res.send('passed');
+       res.send(req.body);
     }
 );
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./config/db');
 const app = express();
 
 //Connect Database
-//connectDB();
+connectDB();
 
 //Init Middleware
 app.use(express.json({extended: false}));
